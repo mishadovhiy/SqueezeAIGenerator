58d38ab2f470669ed8d34720796ae4d6aca7cfa5
Mykhailo Dovhyi <hi@mishadovhiy.com>
2025-07-14 19:54:51 +0300

    Initial Commit

---
+++ SqueezeGenerator/SqueezeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SqueezeGeneratorApp.swift
+//  SqueezeGenerator
+//
+//  Created by Mykhailo Dovhyi on 14.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SqueezeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
