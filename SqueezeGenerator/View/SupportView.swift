59a3a53b5ae06fa305ef8e321ee3162cf1604c07
Misha Dovhiy <44978117+mishadovhiy@users.noreply.github.com>
2025-08-31 21:24:31 +0300

    support ui: not completed

--- SqueezeGenerator/View/SupportView.swift
+++ SqueezeGenerator/View/SupportView.swift
@@ -13,9 +13,13 @@ struct SupportView: View {
 
     var body: some View {
         VStack {
-            field(title: "Header", text: $request.header)
-            field(title: "Title", text: $request.title)
-            field(title: "Message", text: $request.text, placeholder: "Having an issue or want to give an advice?")
+            VStack {
+                field(title: "Header", text: $request.header)
+                field(title: "Title", text: $request.title)
+                field(title: "Message", text: $request.text, placeholder: "Having an issue or want to give an advice?", isLarge: true)
+            }
+            .padding(10)
+            .blurBackground()
             HStack {
                 Spacer().frame(maxWidth: .infinity)
                 Button {
@@ -28,17 +32,39 @@ struct SupportView: View {
                 } label: {
                     Text("Send")
                 }
+                .padding(.vertical, 3)
+                .padding(.horizontal, 8)
+                .blurBackground()
 
             }
+            Spacer()
         }
+        .padding(.horizontal, 10)
         .navigationTitle("Support Request")
         .navigationBarTitleDisplayMode(.large)
     }
 
-    func field(title: String, text: Binding<String>, placeholder: String = "") -> some View {
+    func field(title: String, text: Binding<String>, placeholder: String = "", isLarge: Bool = false) -> some View {
         VStack {
             Text(title)
-            TextField("", text: text, prompt: Text(placeholder))
+            if !isLarge {
+                TextField("", text: text, prompt: Text(placeholder))
+                    .blurBackground()
+            } else {
+                TextEditor(text: text)
+                    .overlay {
+                        if !placeholder.isEmpty && text.wrappedValue.isEmpty {
+                            VStack {
+                                Text(placeholder)
+                                    .multilineTextAlignment(.leading)
+                                    .frame(maxWidth: .infinity)
+                                Spacer()
+                                
+                            }
+                        }
+                    }
+                    .frame(maxHeight: 140)
+            }
         }
     }
 }
